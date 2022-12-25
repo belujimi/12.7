89e24b6051cd6f72d651f1b2a8faa072aca81b55
belujimi <belujimi@belujimi.ru>
2022-12-25 18:50:48 +0500

    bool IskPeriodic

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
-int main()
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IskPeriodic(const string& arr, const int k)
 {
+    if (k < 0)
+    {
+        cout << "Число должно быть большое 0!"<< endl;
+        return false;
+    }
+    string a(k, ' ');
+    if (k <= arr.size())
+    {
+        for (int i = 0; i < k; i++)
+        {
+            a[i] = arr[i];
+        }
+    }
+    else
+    {
+        return false;
+    }
     
 }
